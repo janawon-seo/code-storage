b947181189345960356ad71c6498702d47e7ddb3
jangwonseo <sjw887@naver.com>
2022-09-21 21:11:49 +0900

    계산기 코드를 이용해 try/except를 활용한 심화

---
+++ class7.py
@@ -0,0 +1,32 @@
+
+class Calc():
+    
+    def set_number(self, a, b):
+        self.a = int(a)
+        self.b = int(b)
+            
+    def plus(self):
+        return f"더하기 : {self.a + self.b}"
+    def minus(self):
+        return f"빼  기 : {self.a - self.b}"
+    def multiple(self):
+        return f"곱하기 : {self.a * self.b}"
+    def divide(self):
+        return f"나누기 : {self.a / self.b}"    
+        
+
+    
+
+try:
+    calc = Calc()   
+    calc.set_number(20, 10)
+
+    
+    print(calc.plus())
+    print(calc.minus())
+    print(calc.multiple())
+    print(calc.divide())
+except ValueError:
+    print("숫자만 입력 가능합니다")
+except ZeroDivisionError:
+    print("0으로 나눌 수 없습니다.")   
